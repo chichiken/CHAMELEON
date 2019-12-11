d5e88561026da480e2b247aba403cacc5e79d57f
chichiken <58720123+chichiken@users.noreply.github.com>
2019-12-11 18:20:56 +0900

    구혜진1211
    - 인덱스 전체 레이아웃 배열
    - 인덱스 메인 슬라이더 배너 디자인 1
    - 인덱스 본문 첫 번째 단락(탭, 탭패널)
    - 영화 > 최신개봉작
    - 회원가입

--- asset/js/sub.js
+++ asset/js/sub.js
@@ -19,22 +19,202 @@ $(document).ready(function()
     // FAQ 질문글 이벤트 종료
 
     // Question 이벤트 시작
-    var _quesForm = $("#quesForm .selAreaWrap");
-    var _btn = _quesForm.find("button").first();
+    var _quesForm = $("#quesForm .selFirst");
+    var _quesForm2 = $("#quesForm .selSecond");
+    var _quesForm3 = $("#quesForm .quesType");
     var _ulBtn = $("#quesForm .ulBtn");
-    
-    _btn.on("click",function(e)
+    var _ulBtn2 = $("#quesForm2 .ulBtn");
+    var _ulBtn3 = $("#_quesForm3 .selType");
+    var _btn = _quesForm.find("button").first();
+    var _btn2 = _quesForm2.find("button").first();
+    var btn3 = _quesForm3.find("button").first();
+
+    //문의유형 클릭 이벤트
+    // if ( $(this).is(':animated') ) return false;
+    btn3.on("click",function(e)
     {
         e.preventDefault();
         $(this).next().stop().slideDown();
-        // $(this).toggleClass("active");
+        $(this).toggleClass("active");
+        
 
         $(this).next().on("mouseleave",function()
         {
             $(this).stop().slideUp();
+            $(this).prev().removeClass("active");
         });
+        _ulBtn3.find("button:first, button:last").on("blur", function()
+        {
+            setTimeout(function()
+            {
+                
+                if(!_ulBtn.find("button").is(":focus"))
+                _ulBtn.stop().slideUp(); 
+            },1000);
+        });
+    });
 
-        _quesForm.find("")
+    // 지점, 영화관 선택 이벤트
+    _btn.on("click",function(e)
+    {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        $(this).toggleClass("active");
+
+        $(this).next().on("mouseleave",function()
+        {
+            $(this).stop().slideUp();
+            $(this).prev().removeClass("active");
+        });
+        _ulBtn.find("button:first, button:last").on("blur", function()
+        {
+            setTimeout(function()
+            {
+                if(!_ulBtn.find("button").is(":focus"))
+                _ulBtn.stop().slideUp(); 
+            },1000);
+        });
     })
+    _btn2.on("click",function(e)
+    {
+        e.preventDefault();
+        $(this).next().stop().slideDown();
+        $(this).toggleClass("active");
+
+        $(this).next().on("mouseleave",function()
+        {
+            $(this).stop().slideUp();
+            $(this).prev().removeClass("active");
+        });
+        _ulBtn2.find("button:first, button:last").on("blur", function()
+        {
+            setTimeout(function()
+            {
+                if(!_ulBtn2.find("button").is(":focus"))
+                _ulBtn2.stop().slideUp(); 
+            },1000);
+        });
+    })
+
+    // textarea 글자수 카운트
+    $("#quesTarea").keyup(function()
+    {
+        var print = $(this).val().length;
+        $("#quesTarea + p span").text(print);
+    });
+    
     // Question 이벤트 종료
+
+
+    //모달
+    $('.modal2').featherlight();
+
+   //상단 select date
+            var $rDate=$("#reserve_quick .r_select .r_date")
+            var $sDay=$rDate.find("a").first();
+            var $go=$rDate.find("a").last();
+            var $change;
+
+            $sDay.on("click",function (e){
+                e.preventDefault();
+                $(this).next().stop().slideDown();
+                $(this).next().on("mouseleave",function (){
+                    $(this).stop().slideUp();
+                });
+            });
+
+            $rDate.find("a:first,a:last").on("blur",function(){
+                setTimeout(function  () {
+				if (!$rDate.find("a").is(":focus")) $rDate.find("ul").stop().slideUp();
+			}, 1000);
+            });
+
+            $rDate.find("ul>li>a").on("click",function(e){
+                e.preventDefault();
+                var change=$(this).text();
+                $sDay.focus().text(change).closest("a").next().stop().slideUp();
+            });
+
+
+            //상단 select cinema
+            var $rCinema=$("#reserve_quick .r_select .r_date")
+            var $sDay=$rCinema.find("a").first();
+            var $go=$rCinema.find("a").last();
+            var $change;
+
+            $sDay.on("click",function (e){
+                e.preventDefault();
+                $(this).next().stop().slideDown();
+                $(this).next().on("mouseleave",function (){
+                    $(this).stop().slideUp();
+                });
+            });
+
+            $rCinema.find("a:first,a:last").on("blur",function(){
+                setTimeout(function  () {
+				if (!$rCinema.find("a").is(":focus")) $rCinema.find("ul").stop().slideUp();
+			}, 1000);
+            });
+
+            $rCinema.find("ul>li>a").on("click",function(e){
+                e.preventDefault();
+                var change=$(this).text();
+                $sDay.focus().text(change).closest("a").next().stop().slideUp();
+            });
+
+            //상단 select date
+            var $rDate=$("#reserve_quick .r_select .r_date")
+            var $sDay=$rDate.find("a").first();
+            var $go=$rDate.find("a").last();
+            var $change;
+
+            $sDay.on("click",function (e){
+                e.preventDefault();
+                $(this).next().stop().slideDown();
+                $(this).next().on("mouseleave",function (){
+                    $(this).stop().slideUp();
+                });
+            });
+
+            $rDate.find("a:first,a:last").on("blur",function(){
+                setTimeout(function  () {
+				if (!$rDate.find("a").is(":focus")) $rDate.find("ul").stop().slideUp();
+			}, 1000);
+            });
+
+            $rDate.find("ul>li>a").on("click",function(e){
+                e.preventDefault();
+                var change=$(this).text();
+                $sDay.focus().text(change).closest("a").next().stop().slideUp();
+            });
+
+            //상단 select number
+            var $rNum=$("#reserve_quick .r_select .r_number")
+            var $number=$rNum.find("a").first();
+            var $go=$rNum.find("a").last();
+            var $change2;
+
+            $number.on("click",function (e){
+                e.preventDefault();
+                $(this).next().stop().slideDown();
+                $(this).next().on("mouseleave",function (){
+                    $(this).stop().slideUp();
+                });
+            });
+
+            $rNum.find("a:first,a:last").on("blur",function(){
+                setTimeout(function  () {
+				if (!$rNum.find("a").is(":focus")) $rNum.find("ul").stop().slideUp();
+			}, 1000);
+            });
+
+            $rNum.find("ul>li>a").on("click",function(e){
+                e.preventDefault();
+                var change2=$(this).text();
+
+                $number.focus().text(change2+"명").closest("a").next().stop().slideUp();             
+            }); 
+
+        
+        
 });
